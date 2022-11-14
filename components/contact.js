9a8c5c8fd5bd71a951e7a0d8705654bd63732758
David O <34683034+beyondgoogling@users.noreply.github.com>
2022-11-14 14:26:49 +0100

    Update contact.js
    added contact getter

--- components/contact.js
+++ components/contact.js
@@ -1,3 +1,9 @@
+
+const router = require('express').Router();
+const { MessageMedia, Location } = require("whatsapp-web.js");
+const request = require('request')
+const vuri = require('valid-url');
+const fs = require('fs');
 const router = require('express').Router();
 
 router.get('/getcontacts', (req, res) => {
@@ -17,6 +23,20 @@ router.get('/getcontact/:phone', async (req, res) => {
         });
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.get('/getprofilepic/:phone', async (req, res) => {
     let phone = req.params.phone;
@@ -45,5 +65,43 @@ router.get('/isregistereduser/:phone', async (req, res) => {
         res.send({ status: 'error', message: 'Invalid Phone number' });
     }
 });
+
+router.get('/getnewcontact/:phone', async (req, res) => {
+    let phone = req.params.phone;
+    if (phone == undefined) {
+        res.send({status:"error",message:"please enter valid phone number"});
+    } else {
+        client.on('message', async msg => {
+            let chat = await msg.getChat();
+                if (chat.isGroup || msg.isStatus) {
+                  res.send({status: "success", message: "message is from group"});
+                else {
+                        console.log(msg.body);
+                        const contact = await msg.getContact();
+                        const name = contact.pushname
+                        const number = contact.number
+                        console.log('Name:', name ,'Phone Number:', number);
+
+                    res.send({status:"success", message: 'Name:', name ,'Phone Number:', number});
+                }
+        
+    }
+});
+
+// client.on('message', async msg => {
+//     let chat = await msg.getChat();
+//         if (chat.isGroup || msg.isStatus) {
+//             console.log("Message is from Status or Group");
+//         }
+//         else {
+// 	console.log(msg.body);
+//     const contact = await msg.getContact();
+//     const name = contact.pushname
+//     const number = contact.number
+//     console.log('Name:', name ,'Phone Number:', number);
+//             }
+
+
+
 
 module.exports = router;
